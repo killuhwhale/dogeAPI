823cbee603ebc5bb9a28f277e682f2bc62d6b0e5
andaya92 <andayac@gmail.com>
2021-04-29 03:46:49 -0700

    _

--- dogeCoinDailyTask.py
+++ dogeCoinDailyTask.py
@@ -9,12 +9,12 @@ sched = BlockingScheduler()
 q = Queue(connection=conn)
 
 
-@sched.scheduled_job('interval', minutes=3)
-def timed_job():
-    q = Queue(connection=conn)
-    print(q.enqueue(Twitter().run))
-    print(q.enqueue(Doge().run))
-    print('This job is run every three minutes.')
+# @sched.scheduled_job('interval', minutes=3)
+# def timed_job():
+#     q = Queue(connection=conn)
+#     print(q.enqueue(Twitter().run))
+#     print(q.enqueue(Doge().run))
+#     print('This job is run every three minutes.')
 
 @sched.scheduled_job('cron', day_of_week='mon-sun', hour=17)
 def scheduled_job():
