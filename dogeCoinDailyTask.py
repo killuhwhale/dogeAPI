d8cf1cf154a06b35dfc80943a53145008805bf53
andaya92 <andayac@gmail.com>
2021-04-29 02:44:42 -0700

    _

--- dogeCoinDailyTask.py
+++ dogeCoinDailyTask.py
@@ -11,4 +11,17 @@ def scheduled_job():
     Doge().run()
     print('This job is run every weekday at 5pm.')
 
+
+@sched.scheduled_job('interval', minutes=3)
+def timed_job():
+    Twitter().run()
+    Doge().run()
+    print('This job is run every three minutes.')
+
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=17)
+def scheduled_job():
+    Twitter().run()
+    Doge().run()
+    print('This job is run every weekday at 5pm.')
+
 sched.start()
